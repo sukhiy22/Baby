b50887a3e85a652b2c188ee28939bb0223c6f2bd
sukhiy22 <sukhiyaakter54@gmail.com>
2026-09-26 00:01:51 +0600

    Update age.js

--- Script/commands/age.js
+++ Script/commands/age.js
@@ -93,7 +93,7 @@ module.exports = {
 ┃❖ ${totalDays} Days
 ┃❖ ${totalHours} Hours
 ┣━━━━━━━━━━━━━━━━❂
-┃  𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗯𝘆: ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
+┃  𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗯𝘆: ─꯭─⃝‌‌𝗦𝗨𝗛𝗔𝗡 𝗩𝗛𝗔𝗧 𝗕𝗢𝗧 🤖🔥
 ┗━━━━━━━━━━━━━━━━❂`,
         attachment: fs.createReadStream(avatarPath)
       };
